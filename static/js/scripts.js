f8dbbe2d0e3fadd910acace7edf6a1cbc2f631db
akhilkrishnair <akhilkrishna36912@gmail.com>
2023-05-19 14:09:37 +0530

    bio and skills and social media links included

---
+++ static/js/scripts.js
@@ -0,0 +1,22 @@
+let skill_container = document.getElementsByClassName("skills-container")[0];
+let bio_container = document.getElementsByClassName("bio-container")[0];
+let main_content = document.getElementsByClassName("main-content")[0];
+let bio_p = document.getElementsByClassName("bio")[0];
+let msg_style = document.getElementsByClassName("message-style")[0];
+
+main_content.addEventListener("mouseover",function(event){
+    console.log(event.target.classList[0]);
+    if (event.target.classList[0] === "bio-container"){
+        bio_container.classList.toggle("bio-container-view");
+        bio_p.style.display = "flex"
+        msg_style.classList.toggle("message-view")
+
+    }else{
+        bio_container.classList.remove("bio-container-view");
+        msg_style.classList.remove("message-view")
+        bio_p.style.display = "none"
+    }
+
+});
+
+
